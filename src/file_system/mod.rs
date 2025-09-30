e1943f418f553e4d3cfb4edd458e6f69d28d3196
Anthony Virtuoso <avirtuos@gmail.com>
2025-09-29 22:06:58 -0400

    Removed old approach

--- src/file_system/mod.rs
+++
@@ -1,9 +0,0 @@
-//! file_system module: top-level re-export for file, stripe and chunk submodules.
-
-pub mod file;
-pub mod stripe;
-pub mod chunk;
-
-pub use file::WormFile;
-pub use stripe::WorkStripe;
-pub use chunk::{ChunkHeader, write_chunk, read_chunk_header, read_chunk_full, chunk_path_for};
